032977a5eaf1e4337dde1d4b28252591dd0df1de
RabbitJackTrade <39741388+RabbitJackTrade@users.noreply.github.com>
2025-07-24 09:23:37 -0400

    Update  service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,3 +11,21 @@ self.addEventListener("fetch", e => {
     caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
+const refreshInterval = 60 * 60 * 1000; // 60 minutes in milliseconds
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    self.clients.claim() // Take control of all clients immediately
+  );
+
+  // Set up the periodic refresh
+  setInterval(() => {
+    self.clients.matchAll().then((clients) => {
+      clients.forEach((client) => {
+        client.postMessage({
+          type: 'REFRESH_PAGE'
+        });
+      });
+    });
+  }, refreshInterval);
+});
